7ab7d26d839c2d302d2477099abddc0ed6fe50c8
KITRI <soongon@gmail.com>
2024-05-16 16:13:09 +0900

    initial commit

--- src/main/java/kr/re/kitri/tododemo/repository/TodoRepository.java
+++ src/main/java/kr/re/kitri/tododemo/repository/TodoRepository.java
@@ -1,30 +1,15 @@
 package kr.re.kitri.tododemo.repository;
 
 import kr.re.kitri.tododemo.model.Todo;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class TodoRepository {
-    public List<Todo> selectAllTodos() {
-        // 투두데이터를 디비에서 가져와서 자바타입으로 변환해서 넘겨준다.
-        // List<Todo> 이렇게 넘겨준다.
-        List<Todo> todos = List.of(
-                new Todo(1, "설겆이 하기", false),
-                new Todo(2, "방청소 하기", false),
-                new Todo(3, "저녁 하기", false)
-        );
-        return todos;
-    }
-
-    public Todo selectTodoById(String todoId) {
-        return new Todo(Long.parseLong(todoId), "음악듣기", true);
-
-    }
-
-    public Todo insertTodo(Todo todo) {
-        return todo;
-
-    }
+@Mapper
+public interface TodoRepository {
+    List<Todo> selectAllTodos();
+    Todo selectTodoById(String todoId);
+    Todo insertTodo(Todo todo);
 }
