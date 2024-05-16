d876db44a2b58ff84591dc43ddb9927c0174b971
KITRI <soongon@gmail.com>
2024-05-16 15:12:43 +0900

    initial commit

---
+++ src/main/java/kr/re/kitri/tododemo/configuration/DataSourceConfiguration.java
@@ -0,0 +1,27 @@
+package kr.re.kitri.tododemo.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new HikariDataSource(hikariConfig());
+        System.out.println(dataSource);
+        return dataSource;
+    }
+}
